a4c9e59387f2d53f4b4a6ca572e057d6485fa588
Alec Deason <alec@tinycountry.com>
2020-11-06 11:18:41 -0800

    Remove an unneeded ResMut which makes using maps from thread local systems difficult

--- src/map.rs
+++ src/map.rs
@@ -152,7 +152,7 @@ pub trait TileMap<T: Tile, C: Chunk<T>>:
     /// Adds a `Chunk`, creates a handle and stores it at a coordinate position.
     ///
     /// This is the correct way to add a `Chunk`.
-    fn add_chunk<I: ToIndex>(&mut self, chunk: C, v: I, chunks: &mut ResMut<Assets<C>>) {
+    fn add_chunk<I: ToIndex>(&mut self, chunk: C, v: I, chunks: &mut Assets<C>) {
         let index = v.to_index(self.dimensions().x(), self.dimensions().y());
         let handle = chunks.add(chunk);
         self.send_event(MapEvent::Created {
