d9bea0bd5123d77d2b0d53e19c61ec917f82802b
Joshua J. Bouw <dev@joshuajbouw.com>
2021-02-27 18:37:53 +0700

    change 2d chunk mesh to a 3d chunk mesh

--- src/event.rs
+++ src/event.rs
@@ -21,3 +21,23 @@ pub enum TilemapChunkEvent {
         point: Point2,
     },
 }
+
+#[cfg(feature = "bevy_rapier2d")]
+#[derive(Debug)]
+/// Events that can happen to collisions.
+pub enum TilemapCollisionEvent {
+    /// An event when a collision needs to be spawned.
+    Spawned {
+        /// The chunk point that needs a collision spawned.
+        chunk_point: Point2,
+        /// The point in the chunk that needs a collision spawned.
+        tiles: Vec<Tile<Point3>>,
+    },
+    /// An event when a collision needs to be despawned.
+    Despawned {
+        /// The chunk point that needs a collision spawned.
+        chunk_point: Point2,
+        /// The point in the chunk that needs a collision spawned.
+        tiles: Vec<Tile<Point3>>,
+    },
+}
